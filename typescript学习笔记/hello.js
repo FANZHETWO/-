6d58948b1972ec0d59491789df8df91e0c0e606e
panqiang <panqiang1@midea.com>
2019-11-30 13:59:55 +0800

    update

--- typescript学习笔记/hello.js
+++ typescript学习笔记/hello.js
@@ -1,5 +1,8 @@
 function printLabel(labeledObj) {
     console.log(labeledObj.label);
 }
-var myObj = { size: 10, label: "Size 10 Object" };
+var myObj = {
+    size: 10,
+    label: "Size 10 Object"
+};
 printLabel(myObj);
